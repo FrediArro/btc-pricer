33c36d3ce76d756b24b97c1ce453c19add6f4932
fredi <arro.fredi@gmail.com>
2021-08-18 22:38:37 +0300

    Added price_to_csv function. Closes #2.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import urllib.request
 import json
 import csv
@@ -11,7 +12,21 @@ def price_getter(url):
     return price_history
 
 
+# writes price info to CSV local CSV file
+def price_to_csv(price_info):
+    # checks if the file already exist and if so, then deletes it
+    # to not have any old/wrong info in the file.
+    file_name = "price_history.csv"
+    if os.path.exists(file_name):
+        os.remove(file_name)
+    with open(file_name, "w", newline="") as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(["date", "price"])
+        for key, value in price_info.items():
+            writer.writerow([key, value])
+
 btc_price_usd = "https://api.coindesk.com/v1/bpi/historical/close.json"
 btc_price_eur = "https://api.coindesk.com/v1/bpi/historical/close.json?currency=EUR"
 
-print(price_getter(btc_price_usd))
+price_data = price_getter(btc_price_usd)
+price_to_csv(price_data)
